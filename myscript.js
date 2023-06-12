19f5256fff0a350fbcac48242fe75480cb75d61b
brunotenca <btenca@itba.edu.ar>
2023-06-12 20:13:34 -0300

    can

--- myscript.js
+++ myscript.js
@@ -1,4 +1,11 @@
-let a = 0
-console.log("esto es javaaaaaa")
-let bruno="capo"
-console.log("bruno",bruno)
+const precioGuitarra1= 500000
+const precioGuitarra2=200000
+descuento_1=10
+descuento_2=20
+
+
+let guitarra1= precioGuitarra1*(descuento1/100)
+console.log("Los días martes, hay descuento. La compra sería de $", precioGuitarra1)
+
+let guitarra2= precioGuitarra2*(descuento2/100)
+console.log("Los días jueves, hay descuento. La compra sería de $", precioGuitarra2)
