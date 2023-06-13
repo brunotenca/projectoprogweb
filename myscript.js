57a33859de7f56a5331e5f7fd214d46717d03b4b
brunotenca <btenca@itba.edu.ar>
2023-06-13 19:34:06 -0300

    footer

--- myscript.js
+++ myscript.js
@@ -4,8 +4,13 @@ descuento_1=10
 descuento_2=20
 
 
-let guitarra1= precioGuitarra1*(descuento1/100)
+let guitarra1= precioGuitarra1*(descuento_1/100)
 console.log("Los días martes, hay descuento. La compra sería de $", precioGuitarra1)
 
-let guitarra2= precioGuitarra2*(descuento2/100)
+let guitarra2= precioGuitarra2*(descuento_2/100)
 console.log("Los días jueves, hay descuento. La compra sería de $", precioGuitarra2)
+let edad=0;
+edad= document.getElementsByClassName("").value
+localStorage.setItem("edad_usuario",edad)
+localStorage.getItem("edad_usuario")
+localStorage.clear
